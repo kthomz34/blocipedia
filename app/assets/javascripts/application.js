ff44531df1f27114167313b16d3475ca2f0a2778
Kurt Thomas <kurt.thomas34@gmail.com>
2017-02-28 19:12:09 +0000

    added blocmetrics

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,12 @@
 //= require turbolinks
 //= require bootstrap
 //= require_tree .
+
+var blocmetrics = {};
+  blocmetrics.report = function(eventName){
+    var sale = {sale: { name: eventName }};
+    var request = new XMLHttpRequest();
+    request.open("POST", "https://bloc-foundation-2-agrund.c9users.io/api/events", true);
+    request.setRequestHeader('Content-Type', 'application/json');
+    request.send(JSON.stringify(sale));
+  };
